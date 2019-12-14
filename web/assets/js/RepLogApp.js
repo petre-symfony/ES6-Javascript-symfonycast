c48a245fe523e53c4bb8a4bdc0e8ed0a44c809c7
petrero <petrero@gmail.com>
2019-12-14 17:50:30 +0200

    13.8.Template Strings
    Remove the upper function and put everything back to normal.

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -198,12 +198,15 @@
     }
   }
 
+  /*
   function upper(template, ...expressions) {
     return template.reduce((accumulator, part, i) => {
       return accumulator + (expressions[i - 1].toUpperCase ? expressions[i - 1].toUpperCase() : expressions[i - 1]) + part
     })
   }
-  const rowTemplate = (repLog) => upper`
+  */
+
+  const rowTemplate = (repLog) => /*upper*/`
     <tr data-weight="${repLog.totalWeightLifted}">
       <td>${repLog.itemLabel}</td>
       <td>${repLog.reps}</td>
