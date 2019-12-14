dc7631d5abb8395dcc10cee1a518f2a9529355c0
petrero <petrero@gmail.com>
2019-12-14 16:26:03 +0200

    12.4.The... Spread Operator{Spread Operator for Creating a new Array}
    Spread Operator for Creating a new Array
    
    But the spread operator has one more cool trick. At the bottom, add another variable: let copyOfGreatThings = greatThings. Now, add something else to this new variable: use copyOfGreatThings.push() to add something that we all know is great: summer. At the bottom, console.log(copyOfGreatThings).
    
    Here's the question: we know summer now lives in copyOfGreatThings(). But does it also now live inside of greatThings? Try it! It does! Summer lives in both arrays! And this makes sense: arrays are objects in JavaScript, and just like in PHP, objects are passed by reference. In reality, greatThings and copyOfGreatThings are identical: they both point to the same array in memory.
    
    As we get more advanced, especially in React.js, the idea of not mutating objects will become increasingly important. What I mean is, there will be times when it wil be really important to copy an object, instead of modifying the original.

--- play.js
+++ play.js
@@ -37,6 +37,9 @@ let printThreeThings = function(thing1, thing2, thing3) {
 
 let yummyThings = ['pizza', 'gelato', 'sushi', 'cheeseburger'];
 let greatThings = ['swimming', 'sunsets', ...yummyThings, 'New Orleans'];
+let copyOfGreatThings = greatThings;
+copyOfGreatThings.push('summer');
 
 printThreeThings(...yummyThings);
 console.log(greatThings);
+console.log(copyOfGreatThings);
